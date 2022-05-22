ba2dbe7467b961c378a72da1cbb377194f4cc41a
Stratos <StratosBlue@outlook.com>
2022-05-22 16:41:04 +0800

    add test console.

---
+++ test/caj2pdf.testconsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.IO.Enumeration;
+
+using caj2pdf;
+
+Caj2PdfConverter.Init();
+
+var rootPath = Path.Combine(Environment.CurrentDirectory, "files");
+
+var searchPatterns = new[] { "*.caj", "*.kdh" };
+var files = Directory.EnumerateFiles(rootPath, "*.???", SearchOption.AllDirectories)
+                     .Where(fileName => searchPatterns.Any(pattern => FileSystemName.MatchesSimpleExpression(pattern, fileName)))
+                     .ToArray();
+
+if (files.Length == 0)
+{
+    Console.WriteLine("No files found");
+    return;
+}
+
+foreach (var sourceFile in files)
+{
+    var sw = Stopwatch.StartNew();
+    var outputFile = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(sourceFile))!, "out", Path.ChangeExtension(Path.GetFileName(sourceFile), ".pdf"));
+    Console.WriteLine("Converting {0} to {1}", sourceFile, outputFile);
+    await Caj2PdfConverter.ConvertAsync(sourceFile, outputFile, CancellationToken.None);
+    sw.Stop();
+    Console.WriteLine("{0} -> {1} in {2}", sourceFile, outputFile, sw.Elapsed);
+    Console.WriteLine();
+}
